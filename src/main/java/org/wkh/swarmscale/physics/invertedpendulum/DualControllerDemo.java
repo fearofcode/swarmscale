e6046aea54c1be477eaadfbfe26ca46aa84df9ec
Warren Henning <warren.henning@gmail.com>
2017-03-14 09:33:25 -0700

    i'm done with PSO for now

--- src/main/java/org/wkh/swarmscale/physics/invertedpendulum/DualControllerDemo.java
+++ src/main/java/org/wkh/swarmscale/physics/invertedpendulum/DualControllerDemo.java
@@ -5,7 +5,7 @@ import org.wkh.swarmscale.physics.PhysicalSystemRenderer;
 public class DualControllerDemo {
     public static void main(String[] args) {
         final double[] position = {
-                0.0, 1000.0, 945.3476825963149, 174.37130465230928, 890.1213632434017, 665.647999448197
+                897.2228198264341, 0.0, 785.2912289498331, 0.0, 0.0, 0.0
         };
 
         final double initialRotation = 5.0;
